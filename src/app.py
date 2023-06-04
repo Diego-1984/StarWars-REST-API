590bf8ef5f6e7f63c888550bef74960b8c08fb9e
Diego-1984 <diegomenoruceta@gmail.com>
2023-06-04 16:19:55 +0000

    Faltan 3 end points por terminar

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Favorite, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -47,36 +47,70 @@ def handle_hello():
 
 @app.route ("/people", methods="GET")
 def list_all_people():
-    pass
+    characters = People.query.all()
+    response_people = [people.serialize() for people in characters]
+    response_body ={
+        "msg": "Esta es la lista de personajes"
+    }
+    return jsonify(response_people), 200
 
-@app.route ("people/<int:people_id>", methods="GET")
-def list_one_people(people_id):
-    pass
+@app.route ("user/<int:user_id>", methods="GET")
+def get_one_user(user_id):
+    user = User.query.get(user_id)
+    user = User.serialize()
+    return jsonify(OneUser), 200
+    
+@app.route ("character/<int:character_id>", methods="GET")
+def get_one_user(user_id):
+    user = User.query.get(user_id)
+    user = User.serialize()
+    return jsonify(OneCharacter), 200
 
-@app.route ("/planets", methods="GET")
-def list_all_people():
-    pass
+@app.route ("/planet", methods="GET")
+def list_all_planets():
+    planets = Planet.query.all()
+    response_planets = [planet.serialize() for planet in planets]
+    response_body ={
+        "msg":"Esta es la lista de planetas"
+    }
+    return jsonify(response_planets), 200
 
-app.route ("planets/<int:planets_id>", methods="GET")
-def list_one_people(planets_id):
-    pass
+app.route ("planet/<int:planet_id>", methods="GET")
+def get_one_plantet(planet_id):
+    planet =  Planet.query.get(planet_id)
+    planet = Planet.serialize()
+    return jsonify(OnePlanet), 200
 
-#end points para poder tener usuarios en nuestro blog
 
-@app.route("/users", methods="GET")
+@app.route("/user", methods="GET")
 def list_all_users():
-    pass
+    users = User.query.all()
+    response_user = [user.serialize() for user in users]
+    response_body={
+        "msg":"Esta es la lista de usuarios"
+    }
+    return jsonify(response_users), 200
 
-@app.route("/users/favorites", methods="GET")
-def favorites_per_user():
-    pass
+@app.route("<int:user_id>/favorites", methods="GET")
+def favorites_per_user(user_id):
+    favorite = Favorite.query.filter_by(user_id=user_id).all()
+    favorite_user = [Favorite.serialize() for favorite in favorite]
+    return jsonify(favorite_user), 200
+    
 
-@app.route("/users/favorites<int:planet_id>", methods="POST")
-def add_favorite_planet(id):
-    pass
+@app.route("<int:user_id>/favorite/planet<int:planet_id>", methods="POST")
+def add_favorite_planet(user_id, planet_id):
+    favorite = Favorite (user_id = user_id, planet_id =planet_id)
+    db.session.add(favorite)
+    db.session.commit()
+    response_body={
+        "msg":"Planeta agergado"
+    }
+    return jsonify(response_body), 200
+    
 
 @app.route("/users/favorites<int:people_id>", methods="POST")
-def add_people(id):
+def add_favorite_people(id):
     pass
 
 @app.route("/users/favorites<int:planet_id>", methods="DELETE")
@@ -84,7 +118,7 @@ def delete_favorite_planet(id):
     pass
 
 @app.route("/users/favorites<int:people_id>", methods="POST")
-def delete_people(id):
+def delete_favorite_people(id):
     pass
 
 # this only runs if `$ python src/app.py` is executed
